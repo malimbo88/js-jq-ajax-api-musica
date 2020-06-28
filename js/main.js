78971094629742f8eecdc85e048fc454f567533d
Nicola <nicolavalente88@gmail.com>
2020-06-28 15:45:17 +0200

    Dead Point

--- js/main.js
+++ js/main.js
@@ -8,10 +8,8 @@ $(document).ready(function() {
       success: function(data, state) {
         var musicAlbums = data.response
         //sleziono un genere musicale indicandolo attraverso il form optionSelect
-        //clicco sul button generate per selezionare i brani in base al genere musicale
-        $("#generate").click(function () {
-          htmlAlbums(musicAlbums);
-        });
+        htmlAlbums(musicAlbums);
+
       },
       error: function (request, state, error) {
         alert("Error: " + error);
@@ -27,23 +25,38 @@ $(document).ready(function() {
     var template = Handlebars.compile(source);
     //Variabile che indica il valore di select
     var optionSelect = $("#music_genre").val()
-    //faccio un ciclo per prendere ogni singolo elemento album fornitomi in array
-    for (var i = 0; i < arrayMusicAlbums.length; i++) {
-      //variabile che definisce ogni singolo elemento dell'array musicAlbums
-      //singleMusicAlbum e' un oggetto
-      var singleMusicAlbum = arrayMusicAlbums[i];
-      // definisce il valore della chiave genre di ogni oggetto SingleMusicAlbum
-      var singleAlbumGenre = singleMusicAlbum.genre.toLowerCase()
-      //console.log(singleMusicAlbum);
-      if(optionSelect === singleAlbumGenre) {
-        //Creo il codice html con tutte le informazioni contenute nel mio ogetto singleMusicAlbum
-        var html = template(singleMusicAlbum)
-        //console.log(html);
-        //Stampo il mio html compilato dentro a cds-container
-        $(".cds-container").append(html)
-        $(".cds-container").children().addClass("visible")
-    }
-
-    }
+    //Assegno un valore a selectedMusicGenre
+    //In base al valore scelto mostro una serie di risultati
+    $("select#music_genre").change(function(){
+      //variabile che contiene il valore option di selected
+      console.log($("select#music_genre").val())
+      var selectedMusicGenre = $(this).children("option:selected").val();
+      console.log(selectedMusicGenre)
+      //Variabile che rappresenta il div in cui dovro` stampare i risultati
+      var cdsContainer = $(".cds-container");
+      cdsContainer.html("")
+      //faccio un ciclo per prendere ogni singolo elemento album fornitomi in array
+      for (var i = 0; i < arrayMusicAlbums.length; i++) {
+        //variabile che definisce ogni singolo elemento dell'array musicAlbums
+        //singleMusicAlbum e' un oggetto
+        var singleMusicAlbum = arrayMusicAlbums[i];
+        // definisce il valore della chiave genre di ogni oggetto SingleMusicAlbum
+        var singleAlbumGenre = singleMusicAlbum.genre.toLowerCase()
+        //Stampo tutti i risultati indipendentemente dal genre musicale
+        if (selectedMusicGenre === "all") {
+          //Creo il codice html con tutte le informazioni contenute nel mio ogetto singleMusicAlbum
+          var html = template(singleMusicAlbum)
+          //Stampo il mio html compilato dentro a cds-container
+           cdsContainer.append(html)
+        }
+        //Stampo solo i risultati che hanno come genre musicale quello indicato in select input
+        else if(selectedMusicGenre === singleAlbumGenre) {
+          //Creo il codice html con tutte le informazioni contenute nel mio ogetto singleMusicAlbum
+          var html = template(singleMusicAlbum)
+          //Stampo il mio html compilato dentro a cds-container
+           cdsContainer.append(html)
+        }
+      }
+    });
   }
 });
